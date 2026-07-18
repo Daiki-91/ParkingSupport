481dddbb496cfb823ba00d4e9e29bcf517326831
Daiki Maeda <daiki.m9191@gmail.com>
2026-07-18 17:50:27 +0900

    Initial Commit

---
+++ ParkingSupport/ParkingSupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingSupportApp.swift
+//  ParkingSupport
+//
+//  Created by DaikiMaeda on 2026/07/18.
+//
+
+import SwiftUI
+
+@main
+struct ParkingSupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

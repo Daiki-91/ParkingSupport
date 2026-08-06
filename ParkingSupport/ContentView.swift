00092a66933643199e6db2a2be212092fcb9f55b
daiki-91 <daiki.m9191@gmail.com>
2026-08-06 22:38:09 +0900

    Timerによる自動更新機能と補正値のリセットボタンを実装

--- ParkingSupport/ContentView.swift
+++ ParkingSupport/ContentView.swift
@@ -6,22 +6,26 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     // @State: この値が変わったら画面も自動で再描画される、という印
     // private: このView(ContentView)の中だけで使う変数、という制限
-    @State private var entryTime = Date()   // 入庫時刻(初期値は今の時刻)
+    // 入庫時刻(初期値は今の時刻)
+    @State private var entryTime = Date()
+    // 補正分数(-15〜+15分を想定)
+    @State private var adjustmentMinutes: Double = 0
     
-    @State private var adjustmentMinutes: Double = 0  // 補正分数(-15〜+15分を想定)
     @State private var showAlert = false
+    // 「今の時刻」を保持する変数を新設
+    @State private var currentTime = Date()
     
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        let now = Date()  // 現在時刻を取得
-        // entryTime(入庫時刻)からnow(現在時刻)までの経過時間を「秒」で計算
-        let elapsedSeconds = now.timeIntervalSince(entryTime)
+        let elapsedSeconds = currentTime.timeIntervalSince(entryTime)
         // 秒を分に変換(60秒 = 1分)
-        let elapsedMinutes = elapsedSeconds / 60 + adjustmentMinutes
+        let elapsedMinutes = max(0, elapsedSeconds / 60 + adjustmentMinutes)
         // 分を時間に変換し、切り上げる(例: 61分 → 1.01時間 → 切り上げで2時間扱い)
         // 今使ってるカレンダー(地域設定)を取得
         let calender = Calendar.current
@@ -30,7 +34,7 @@ struct ContentView: View {
         
         let ratePerHour = (hour >= 8 && hour < 20) ? 200 : 100
         
-        let hours = ceil(elapsedMinutes / 60)
+        let hours = max(1,ceil(elapsedMinutes / 60))
         // 時間数 × 時間あたり料金 = 合計料金
         let fee = hours * Double(ratePerHour)
         
@@ -55,6 +59,10 @@ struct ContentView: View {
                 
                 Slider(value: $adjustmentMinutes, in: -15...15, step: 1)
                 
+                Button("補正をリセット"){
+                    adjustmentMinutes = 0
+                }
+                
                 Text("補正: \(Int(adjustmentMinutes))分")
                 
                 Button("アラートテスト") {
@@ -65,6 +73,9 @@ struct ContentView: View {
                 .foregroundStyle(Color.white)
             }
             .preferredColorScheme(.dark)
+            .onReceive(timer) { _ in
+                currentTime = Date()
+            }
             .alert("まもなく値上がりします",isPresented: $showAlert) {
                 Button("OK") {}
             }
